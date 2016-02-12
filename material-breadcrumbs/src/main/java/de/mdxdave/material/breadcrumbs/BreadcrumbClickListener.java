6dbf2f6dde40ef9770dbe9fd6342fa3b507263ce
David Kurz <dk@nevoxo.com>
2016-02-12 03:49:58 +0100

    Fix

---
+++ material-breadcrumbs/src/main/java/de/mdxdave/material/breadcrumbs/BreadcrumbClickListener.java
@@ -0,0 +1,9 @@
+package de.mdxdave.material.breadcrumbs;
+
+/**
+ * Created by Dave on 16.11.2015.
+ * Package: de.mdxdave.material.breadcrumbs
+ */
+public interface BreadcrumbClickListener {
+    public void onClick(NavigationItem navigationItem);
+}
